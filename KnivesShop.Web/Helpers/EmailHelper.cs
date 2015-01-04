464a88525b67bca20c138f00042a551e6c17a920
tankovski2 <tankovski.stoian@gmail.com>
2015-01-04 17:40:58 +0200

    Change EmailHelper

--- KnivesShop.Web/Helpers/EmailHelper.cs
+++ KnivesShop.Web/Helpers/EmailHelper.cs
@@ -15,13 +15,9 @@ namespace KnivesShop.Web.Helpers
     {
         #region Constants
 
-        private const string AppEmail = "stankovski@yabolka.com";
-        private const string AppEmailPass = "system1986";
-        private const string AppEmailHostName = "mail.yabolka.com";
-        private const int AppEmailHostPort = 26;
-
         private const string MERCHANT_MAIL_KEY = "MerchantMail";
         private const string MERCHANT_RECEIVING_MAIL_KEY = "MerchantReceivingMail";
+		
         private const string CLIENT_MAIL_TEMPLATE_VIEW = "ClientMail";
         private const string MERCHANT_MAIL_TEMPLATE_VIEW = "MerchantMail";
 
@@ -29,25 +25,6 @@ namespace KnivesShop.Web.Helpers
 
         public static void MakeOrder(OrderArticleViewModel orderModel, IEnumerable<ArticleMailViewModel> orderedItems)
         {
-            //MailAddress fromMail = new MailAddress(AppEmail);
-            //MailAddress toMail = new MailAddress("ov4arq@abv.bg");
-            //MailMessage message = new MailMessage(fromMail, toMail);
-
-            //message.Subject = "Order from "+orderModel.ClientName;
-            //string orderBody = string.Format("Article name: {1}{0}Client name: {2}{0}Client e-mail: {3}{0}" +
-            //"Client phone: {4}{0}Client address: {5}{0}Additional info: {6}{0}", Environment.NewLine, orderModel.ArticlesNames,
-            //orderModel.ClientName, orderModel.ClientEmail, orderModel.ClientPhone, orderModel.ClientAddress, orderModel.AdditionalInfo);
-            //message.Body = orderBody;
-            //message.IsBodyHtml = false;
-
-            //message.SubjectEncoding = System.Text.Encoding.UTF8;
-            //message.BodyEncoding = System.Text.Encoding.UTF8;
-
-            //SmtpClient smtp = new SmtpClient(AppEmailHostName, AppEmailHostPort);
-            //smtp.Credentials = new NetworkCredential(AppEmail, AppEmailPass);
-
-            //smtp.Send(message);
-
             SendMailToMerchant(orderModel);
             SendMailToClient(orderModel, orderedItems);
         }
